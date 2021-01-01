9c51c2b8decd3bff4aee0813cf7c7625401d2dee
zunopark <enzoribery@naver.com>
2021-01-01 16:09:34 +0900

    feat: add router

---
+++ README.md
@@ -0,0 +1,7 @@
+# 나만의 넷플릭스, Zunflix
+
+## 화면 구성
+- [ ] Home
+- [ ] TV Shows
+- [ ] Search
+- [ ] Detail
---
+++ src/Components/Header.js
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from "react";
+
+export default () => (
+  <header>
+    <ul>
+      <li>
+        <a href="/">Movies</a>
+      </li>
+      <li>
+        <a href="/tv">TV</a>
+      </li>
+      <li>
+        <a href="/search">Search</a>
+      </li>
+    </ul>
+  </header>
+);
