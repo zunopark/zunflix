d549448985dd314fca9701f8aa28418354ab9ded
zunopark <enzoribery@naver.com>
2021-01-01 17:05:39 +0900

    feat: add css to the header

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -9,14 +9,18 @@ import {
 import Home from "../Routes/Home";
 import Search from "../Routes/Search";
 import TV from "../Routes/TV";
+import Header from "../Components/Header";
 
 export default () => (
   <Router>
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/tv" component={TV} />
-      <Route path="/search" component={Search} />
-      <Redirect from="*" to="/" />
-    </Switch>
+    <>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/tv" component={TV} />
+        <Route path="/search" component={Search} />
+        <Redirect from="*" to="/" />
+      </Switch>
+    </>
   </Router>
 );
