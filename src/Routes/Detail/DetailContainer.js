3e0a42dc33c64b93a2b92d170103393c446176bd
zunopark <enzoribery@naver.com>
2021-01-03 21:01:40 +0900

    feat: done with container

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -2,13 +2,49 @@
 /* eslint-disable react/require-render-return */
 import React from "react";
 import DetailPresenter from "./DetailPresenter";
+import { movieApi, tvApi } from "../../api";
 
 export default class extends React.Component {
-  state = {
-    result: null,
-    error: null,
-    loading: true,
-  };
+  constructor(props) {
+    super(props);
+    const {
+      location: { pathname },
+    } = props;
+    this.state = {
+      result: null,
+      error: null,
+      loading: true,
+      isMovie: pathname.includes("/movie/"),
+    };
+  }
+
+  async componentDidMount() {
+    const {
+      match: {
+        params: { id },
+      },
+      history: { push },
+    } = this.props;
+    const { isMovie } = this.state;
+    const parsedId = parseInt(id);
+    if (isNaN(parsedId)) {
+      return push("/");
+    }
+    let result = null;
+    try {
+      if (isMovie) {
+        ({ data: result } = await movieApi.movieDetail(parsedId));
+      } else {
+        ({ data: result } = await tvApi.tvDetail(parsedId));
+      }
+      console.log(result);
+      this.setState({ result, loading: false });
+    } catch (err) {
+      this.setState({ error: "Can't find anythings" });
+    } finally {
+      this.setState({ loading: false, result });
+    }
+  }
 
   render() {
     const { result, error, loading } = this.state;
