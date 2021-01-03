ae0491610a45eb5cbb99fc68754754e7b1e9376c
zunopark <enzoribery@naver.com>
2021-01-03 17:57:48 +0900

    feat: add TV container

--- src/Routes/TV/TvContainer.js
+++ src/Routes/TV/TvContainer.js
@@ -2,6 +2,7 @@
 /* eslint-disable react/require-render-return */
 import React from "react";
 import TvPresenter from "./TvPresenter";
+import { tvApi } from "../../api";
 
 export default class extends React.Component {
   state = {
@@ -11,6 +12,29 @@ export default class extends React.Component {
     error: null,
     loading: true,
   };
+
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: topRated },
+      } = await tvApi.topRated();
+      const {
+        data: { results: popular },
+      } = await tvApi.popular();
+      const {
+        data: { results: airingToday },
+      } = await tvApi.airingToday();
+      this.setState({
+        topRated,
+        popular,
+        airingToday,
+      });
+    } catch (err) {
+      this.setState({ error: "Can't find TV information." });
+    } finally {
+      this.setState({ loading: false });
+    }
+  }
 
   render() {
     const { topRated, popular, airingToday, error, loading } = this.state;
