bf59c206bfc68052e1e3d9e0849cb0658d4ad2e0
zunopark <enzoribery@naver.com>
2021-01-06 23:01:25 +0900

    feat: add loaders

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,8 +1,34 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Styled from "styled-components";
+import styled from "styled-components";
+import Section from "../../Components/Section";
+import Loader from "../../Components/Loader";
 
-const HomePresenter = ({ nowPlaying, upcoming, popular, loading, error }) => {};
+const Container = styled.div`
+  padding: 0px 10px;
+`;
+
+const HomePresenter = ({ nowPlaying, upcoming, popular, loading, error }) => {
+  return loading ? (
+    <Loader />
+  ) : (
+    <Container>
+      {nowPlaying && nowPlaying.length > 0 && (
+        <Section title="Now Playing">
+          {nowPlaying.map((movie) => movie.title)}
+        </Section>
+      )}
+      {upcoming && upcoming.length > 0 && (
+        <Section title="Upcoming">
+          {upcoming.map((movie) => movie.title)}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title="Popular">{popular.map((movie) => movie.title)}</Section>
+      )}
+    </Container>
+  );
+};
 
 HomePresenter.prototype = {
   nowPlaying: PropTypes.array,
--- src/Routes/TV/TvPresenter.js
+++ src/Routes/TV/TvPresenter.js
@@ -1,8 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Styled from "styled-components";
+import styled from "styled-components";
+import Section from "../../Components/Section";
+import Loader from "../../Components/Loader";
 
-const TvPresenter = ({ topRated, popular, airingToday, loading, error }) => {};
+const Container = styled.div`
+  padding: 0px 10px;
+`;
+
+const TvPresenter = ({ topRated, popular, airingToday, loading, error }) => {
+  return loading ? (
+    <Loader />
+  ) : (
+    <Container>
+      {topRated && topRated.length > 0 && (
+        <Section title="Top Rated Shows">
+          {topRated.map((show) => show.name)}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title="Popular Shows">
+          {popular.map((show) => show.name)}
+        </Section>
+      )}
+      {airingToday && airingToday.length > 0 && (
+        <Section title="Airing Today">
+          {airingToday.map((show) => show.name)}
+        </Section>
+      )}
+    </Container>
+  );
+};
 
 TvPresenter.prototype = {
   topRated: PropTypes.array,
